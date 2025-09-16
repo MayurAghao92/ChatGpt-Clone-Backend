ada6cc02aae8ddf3754c63faff75c427e8be200b
MayurAghao92 <mayuraghao54@gmail.com>
2025-09-16 17:01:46 +0530

    Connected Database

---
+++ src/db/db.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';    
+
+async function connectDB(){
+   try{
+     await mongoose.connect(process.env.MONGO_URI)
+   }catch(err){
+     console.log("Error in DB connection", err);
+   }
+}
