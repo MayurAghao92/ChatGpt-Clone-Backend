c4cc5126185017444937ffb9138de06612aca4fe
MayurAghao92 <mayuraghao54@gmail.com>
2025-09-17 16:11:03 +0530

    Added Login Controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,3 +33,36 @@ async function registerController(req, res){
         },
     });
 }
+
+async function loginController(req, res){
+    const{email, password}=req.body;
+
+    const user= await userModel.findOne({
+        email,
+    });
+
+    if(!user){
+        return res.status(400).json({
+            message: "User does not exist",
+        });
+    }
+    const isPasswordValid= await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+        return res.status(400).json({
+            message: "Invalid password",
+        });
+    }
+    const token= jwt.sign({id: user._id}, process.env.JWT_SECRET);
+    res.cookie("token", token);
+
+    return res.status(200).json({
+        message: "User logged in successfully",
+        user:{
+            id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+        },
+    });
+}
+
+export {registerController, loginController};
