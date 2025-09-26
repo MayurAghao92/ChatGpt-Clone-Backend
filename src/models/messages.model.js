dab9096a969062c24e444e2d30e6bb2f1a1aff43
MayurAghao92 <mayuraghao54@gmail.com>
2025-09-26 19:39:45 +0530

    Created message Schema and Added vector database

---
+++ src/models/messages.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    chat:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'chat'
+    },
+    content:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum: ['user','model','system'],
+        required: true,
+    }
+},{timestamps: true});  
+
+const messageModel= mongoose.model('message', messageSchema);
+
+export default messageModel;
--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,48 +1,74 @@
 import { Server } from "socket.io";
-import cookie from 'cookie';
+import cookie from "cookie";
 import userModel from "../models/user.model.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import generateAIResponse from "../services/ai.service.js";
+import messageModel from "../models/messages.model.js";
 
- function setupSocketServer(httpServer) {
-    const io = new Server(httpServer, {});
+function setupSocketServer(httpServer) {
+  const io = new Server(httpServer, {});
 
-    io.use(async(socket, next) => {
-        const cookies=cookie.parse(socket.handshake.headers?.cookie ||'');
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
 
-        if(!cookies.token){
-            next(new Error("Authentication error:No token provided"));
-        }
+    if (!cookies.token) {
+      next(new Error("Authentication error:No token provided"));
+    }
 
-        try{
-            const decoded=jwt.verify(cookies.token,process.env.JWT_SECRET);
-            const user=await userModel.findById(decoded.id);
-            socket.user=user;
-            next();
-        }catch(err){
-            next(new Error("Authentication error:Invalid token"));
-        }
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded.id);
+      socket.user = user;
+      next();
+    } catch (err) {
+      next(new Error("Authentication error:Invalid token"));
+    }
+  });
 
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("ai-message", async (messsagePayload) => {
+
+      await messageModel.create({
+        user: socket.user._id,
+        chat: messsagePayload.chat,
+        content: messsagePayload.message,
+        role: "user",
+      });
+
+      const chatHistory = (
+        await messageModel
+          .find({
+            chat: messsagePayload.chat,
+          })
+          .sort({ createdAt: -1 })
+          .limit(10)
+          .lean()
+      ).reverse();
+
+      const response = await generateAIResponse(
+        chatHistory.map(msg => {
+          return {
+            role: msg.role,
+            parts: [{ text: msg.content }],
+          };
+        })
+      );
+
+      await messageModel.create({
+        user: socket.user._id,
+        chat: messsagePayload.chat,
+        content: response,
+        role: "model",
+      });
+
+      socket.emit("ai-response", {
+        content: response,
+        chat: messsagePayload.chat,
+      });
     });
-
-    io.on("connection", (socket) => {
-        console.log("A user connected:", socket.id);
-
-        socket.on("disconnect", () => {
-            console.log("A user disconnected:", socket.id);
-        });
-
-        socket.on("ai-message", async(messsagePayload) => {
-            console.log("Message received:", messsagePayload);
-
-            const response=await generateAIResponse(messsagePayload.message);
-            socket.emit("ai-response", {
-                content: response,
-                chat: messsagePayload.chat
-            })
-        });
-    });
+  });
 }
-
 
 export default setupSocketServer;
