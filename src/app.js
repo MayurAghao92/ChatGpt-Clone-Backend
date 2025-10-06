69f6efb8506af59e9a26a14f435c798016f0bb59
MayurAghao92 <mayuraghao54@gmail.com>
2025-10-06 17:39:15 +0530

    Fixed issue

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import authRoutes from './routes/auth.routes.js';
 import chatRoutes from './routes/chat.routes.js';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from "url";
+
+// recreate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
