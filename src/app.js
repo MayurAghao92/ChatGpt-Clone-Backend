b565e0ba2f1e0cc36b4902d54bcac093515d7a50
MayurAghao92 <mayuraghao54@gmail.com>
2025-10-06 17:34:20 +0530

    Edited App.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
