88a03910a8b8c09f449baf04b48461e63dbb8cde
MayurAghao92 <mayuraghao54@gmail.com>
2026-02-22 17:08:08 +0530

    Refactor Pinecone client initialization to use environment variable for API key

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from "url";
 
-// recreate __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
